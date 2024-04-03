937214896838c2748a134a53ee20f431d151307c
Mazon <mishamakz@mail.ru>
2024-04-03 23:00:52 +0300

    Improved readability

--- Scripts/Bus.cs
+++ Scripts/Bus.cs
@@ -19,28 +19,42 @@ class BusInfo
 
         int peopleExitingBus = _random.Next(0, _places + 1);
 
-        if (peopleExitingBus > 0)
-        {
-            Console.WriteLine($"{peopleExitingBus} человек вышли на {_stopNumber} остановке");
-            _places -= peopleExitingBus;
-        }
+        ManagePassengersExitingBus(peopleExitingBus);
 
         int peopleBoardingBus = _random.Next(0, _maxCapacity - _places + 1);
         _places += peopleBoardingBus;
 
-        if (_places > _maxCapacity)
-        {
-            Console.WriteLine($"На {_stopNumber} остановке {_places - _maxCapacity} человек не могут войти в автобус, так как он заполнен");
-            _places = _maxCapacity;
-        }
+        CheckAndAdjustCapacity(_places);
 
-        if (_places < 0)
-        {
-            Console.WriteLine($"На {_stopNumber} остановке {_places} человек вышли из автобуса, хотя там не было ни одного пассажира");
-            _places = 0;
-        }
+        UpdatePassengerCount(_places);
 
         Console.WriteLine($"{peopleBoardingBus} человек зашли на остановке {_stopNumber} и мест стало {_places}");
         Console.WriteLine(new string('-', 50));
+    }
+
+    private void ManagePassengersExitingBus(int passengers)
+    {
+        if (passengers > 0)
+        {
+            Console.WriteLine($"{passengers} человек вышли на {_stopNumber} остановке");
+            _places -= passengers;
+        }
+    }
+
+    private void CheckAndAdjustCapacity(int places)
+    {
+        if (places > _maxCapacity)
+        {
+            Console.WriteLine($"На {_stopNumber} остановке {places - _maxCapacity} человек не могут войти в автобус, так как он заполнен");
+            _places = _maxCapacity;
+        }
+    }
+
+    private void UpdatePassengerCount(int places)
+    {
+        if (places < 0)
+        {
+            Console.WriteLine($"На {_stopNumber} остановке {places} человек вышли из автобуса, хотя там не было ни одного пассажира"); _places = 0;
+        }
     }
 }
